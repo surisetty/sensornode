250c9c0f9a7461462a4bafd9fe7926bb7607f2cb
naveen <naveenmailid@gmail.com>
2018-03-10 06:42:51 -0800

    4 commit

--- codec.sh
+++ codec.sh
@@ -1,11 +1,13 @@
 
 PASSWORD="linear"
+
 if [ "$1" == "e" ]; then
    echo "Encrypting"
-   for FILE in `find .  -iname "*.py"`
+   for FILE in `find ./ -type f \( -iname \*.py -o -iname \*.txt \)`
    do 
        
        gpg --yes --batch --passphrase=${PASSWORD} -c ${FILE}
+       rm -f ${FILE}
 
    done 
    
@@ -13,10 +15,11 @@ fi
 
 if [ "$1" == "d" ]; then
    echo "Decrypting"
-   for FILE in `find .  -iname "*.py.gpg"`
+   for FILE in `find ./ -type f \( -iname \*.py.gpg -o -iname \*.txt.gpg \)`
    do 
        
        gpg --yes --batch --passphrase=${PASSWORD}  ${FILE}
+       rm -f ${FILE}
 
    done 
 fi
